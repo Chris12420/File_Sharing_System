2c8aa96e641332c7524e301b876fe0161c8c49aa
Chris12420 <chrisworks1102@gmail.com>
2025-04-18 01:28:44 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace with your GitHub repository name
-const repositoryName = 'File_Sharing_System'; // <-- **Enter your repository name here**
-
 // https://vitejs.dev/config/
-export default defineConfig(({
-  command
-}) => ({
+export default defineConfig({
   plugins: [react()],
-  // --- Add base configuration ---
-  base: command === 'build' ?
-    `/${repositoryName}/` // Use repository name as base path for production build
-    :
-    '/', // Use root path for development
-  // --- End base configuration ---
   server: {
     port: 3000, // You can specify the port for the dev server
     open: true, // Automatically open the app in the browser
   },
-})); 
+}); 
